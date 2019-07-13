00c957588ac58861d887e6218c940d42a989e552
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2019-07-13 20:17:57 +0900

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
-apt-get install -y libmicrohttpd-dev libjansson-dev libnice-dev \
-    libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
+apt-get install -y libcrypto++-dev libconfig-dev libmicrohttpd-dev libjansson-dev libnice-dev \
+    libssl-dev libsofia-sip-ua-dev libglib2.34-dev \
     libopus-dev libogg-dev libini-config-dev libcollection-dev \
     pkg-config gengetopt libtool automake
